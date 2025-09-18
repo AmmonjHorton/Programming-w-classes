bd433c25354b4872bea6d7d3f71547d92bd7ef09
AmmonjHorton <ammonjh04@gmail.com>
2025-09-18 11:36:02 -0600

    names

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write($"What is your name?");
+        string name = Console.ReadLine();
+        Console.Write($"What is your last name?");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Hello {name} {lastName}!");
     }
 }
