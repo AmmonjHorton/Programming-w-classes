c80248fe4e99cd84f017311dd38c09fc55bd5002
AmmonjHorton <ammonjh04@gmail.com>
2025-09-19 21:01:58 -0600

    percentage stufff

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write($"What is your name?");
-        string name = Console.ReadLine();
-        Console.Write($"What is your last name?");
-        string lastName = Console.ReadLine();
-        Console.WriteLine($"Hello {name} {lastName}!");
+        // Console.Write($"What is your name? ");
+        // string name = Console.ReadLine();
+        // Console.Write($"What is your last name? ");
+        // string lastName = Console.ReadLine();
+        // Console.WriteLine($"Hello {name} {lastName}!");
+
+        
+        Console.Write($"What is your grade percentage? ");
+        string input = Console.ReadLine();
+        double percentage = double.Parse(input);
+        
+        if (percentage >= 90)
+        {
+            Console.WriteLine($"You have an A.");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine($"You have a B.");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine($"You have a C.");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine($"You have a D.");
+        }
+        else
+        {
+            Console.WriteLine($"You have an F.");
+        }
+
     }
 }
