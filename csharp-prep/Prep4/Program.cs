9190bd45fc7640371b10a01d1df79dda581db760
AmmonjHorton <ammonjh04@gmail.com>
2025-09-25 13:23:02 -0600

    lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter any integer onto the list (0 to stop): ");
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number != 0)
+                numbers.Add(number);
+            else
+            {
+                Console.WriteLine($"The sum is {numbers.Sum()}");
+                Console.WriteLine($"The average is {numbers.Average()}");
+                Console.WriteLine($"The largest number is {numbers.Max()}");
+                Console.WriteLine($"The smallest number is {numbers.Min()}");
+                Console.WriteLine("The numbers in order are: ");
+                numbers.Sort();
+                foreach (int num in numbers)
+                    Console.WriteLine(num);
+                break;
+            }
+        }
     }
 }
